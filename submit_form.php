14597f0efdde38312629f813d49dde530d6dae93
Sandeep Vaz <vazsandep@gmail.com>
2024-02-10 23:45:46 +0530

    First commit of the web project

---
+++ submit_form.php
@@ -0,0 +1,37 @@
+<?php
+// Database connection parameters
+$servername = "your_mysql_server";
+$username = "your_mysql_username";
+$password = "your_mysql_password";
+$dbname = "your_database_name";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve form data
+$jobTitle = $_POST['jobTitle'];
+$jobCategory = $_POST['jobCategory'];
+$jobDescription = $_POST['jobDescription'];
+$requirements = $_POST['requirements'];
+$applicationDeadline = $_POST['applicationDeadline'];
+$contactEmail = $_POST['contactEmail'];
+$contactPhone = $_POST['contactPhone'];
+
+// Insert data into the database
+$sql = "INSERT INTO jobs (jobTitle, jobCategory, jobDescription, requirements, applicationDeadline, contactEmail, contactPhone) 
+        VALUES ('$jobTitle', '$jobCategory', '$jobDescription', '$requirements', '$applicationDeadline', '$contactEmail', '$contactPhone')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Job details submitted successfully!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close connection
+$conn->close();
+?>
